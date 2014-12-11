7405c6e61902fc30fae02596cae09dbdb05cdcf3
surpass1 <paulkawalya@yahoo.com>
2014-12-11 15:07:21 +0300

    cleaned index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   def index
   	@homes = Home.where(category: 'introduction')
   	@ministry = Home.where(category: 'ministry')
+  	@aids = Home.where(category: 'news')
   end
 end
